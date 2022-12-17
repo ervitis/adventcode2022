1de912f0d97f02e015dc3d61da19a951c73fbc0b
ervitis <vitomarti@gmail.com>
2022-12-18 00:06:47 +0900

    feat: day 9 - 2

---
+++ day9/largerrope/main.py
@@ -0,0 +1,34 @@
+def main():
+    tx, ty = 0, 0
+    num_pos = set()
+    num_pos.add((tx, ty))
+    directions = {'R': (1, 0), 'D': (0, -1), 'U': (0, 1), 'L': (-1, 0)}
+    knots = [[0, 0] for _ in range(10)]
+
+    with open('../data', 'r') as fp:
+        while True:
+            line = fp.readline().strip()
+            if not line:
+                break
+            direction, steps = line.split(' ')
+            steps = int(steps)
+            while steps > 0:
+                x, y = directions[direction]
+                knots[0][0] += x
+                knots[0][1] += y
+                
+                for i in range(1, 10):
+                    hx, hy = knots[i - 1]
+                    tx, ty = knots[i]
+                    if not (abs(hx - tx) <= 1 and abs(hy - ty) <= 1):
+                        tx += 0 if tx == hx else (hx - tx) // abs(hx - tx)
+                        ty += 0 if ty == hy else (hy - ty) // abs(hy - ty)
+                    knots[i] = [tx, ty]
+                    num_pos.add(tuple(knots[-1]))
+                steps -= 1
+    print(len(num_pos))
+
+
+if __name__ == '__main__':
+    main()
+    
